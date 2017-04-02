f48fd2e926a7bd90298372ffb82a94e444555520
John Buckley <john.buckley7@mail.dcu.ie>
2017-04-02 17:57:37 +0100

    camera button added

--- SDAProjectJohnBuckley/app/src/main/java/com/example/john/sdaprojectjohnbuckley/AddGuestsActivity.java
+++ SDAProjectJohnBuckley/app/src/main/java/com/example/john/sdaprojectjohnbuckley/AddGuestsActivity.java
@@ -1,29 +1,30 @@
 package com.example.john.sdaprojectjohnbuckley;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class AddGuestsActivity extends AppCompatActivity
-{
+
+public class AddGuestsActivity extends AppCompatActivity {
 
     EditText guestnameEntered;
     EditText guestemailEntered;
     EditText guestphonenumberEntered;
-    SQLiteDatabase guest_db = null;
+    private SQLiteDatabase database;
+    private SQLHelper dbHelper;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_guests);
 
         guestnameEntered = (EditText) findViewById(R.id.guestname);
         guestemailEntered = (EditText) findViewById(R.id.guestemail);
         guestphonenumberEntered = (EditText) findViewById(R.id.guestphone);
-
-
     }
+
+
+
 }
--- SDAProjectJohnBuckley/app/src/main/java/com/example/john/sdaprojectjohnbuckley/MainActivity.java
+++ SDAProjectJohnBuckley/app/src/main/java/com/example/john/sdaprojectjohnbuckley/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.john.sdaprojectjohnbuckley;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.View.OnClickListener;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.support.v4.app.Fragment;
 
-
-public class MainActivity extends AppCompatActivity  {
-
+public class MainActivity extends AppCompatActivity
+{
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,7 +35,6 @@ public class MainActivity extends AppCompatActivity  {
                 startActivity(invitesIntent);
             }
         });
-
         Button guestlistButton = (Button) findViewById(R.id.button2);
         guestlistButton.setOnClickListener(new View.OnClickListener()
         {
@@ -34,6 +44,56 @@ public class MainActivity extends AppCompatActivity  {
                 startActivity(invitesIntent);
             }
         });
+        Button cameraButton = (Button) findViewById(R.id.button3);
+        cameraButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
+        {
+            galleryAddPic();
+        }
+    }
+    private File createImageFile() throws IOException
+    {
+        File storageDir = Environment.getExternalStorageDirectory();
+        File image = File.createTempFile("example",  /* prefix */".jpg",         /* suffix */storageDir      /* directory */);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    private void dispatchTakePictureIntent()
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+        {
+            File photoFile = null;
+            try
+            {
+                photoFile = createImageFile();
+            } catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+    private void galleryAddPic()
+    {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
 }
